65f32050b05758b80f7dcf49ad2d38760884f856
Redo76 <redouane.hamadache1@gmail.com>
2022-04-07 16:38:20 +0200

    en coursde creation d'une fonction permettant de recuperer les valeurs du formulaire

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-// Déclaration des variables:
+// Déclaration des variables :
 const backdrop = document.getElementById("backdrop");
 const addMovie = document.getElementById("addMovie");
 const modal = document.getElementById("add-modal");
 const cancel = document.getElementsByClassName("btn--passive");
-const title = document.getElementById("title");
-const imageUrl = document.getElementById("image-url");
+const add = document.querySelector(".btn--success");
 
 
+
+const warning = document.createElement("div");
+
+// Déclaration des fonctions :
 function removeModal() {
     modal.style.display = "none";
     backdrop.style.display = "none";
 }
 
+function ajouterMovie() {
+    let title = document.getElementById("title").value;
+    let imageUrl = document.getElementById("image-url").value;
+    let rating = document.getElementById("rating").value;
+    
+}
+// Début du code :
+
+/*Lorsqu'on clique sur ADD MOVIE la modale ainsi que le fond transparent apparait.*/
 addMovie.addEventListener("click", () =>{
     backdrop.style.display = "block";
     modal.style.display = "block";
 })
 
+/* Lorqu'on clique sur le bouton cancel de la modale, on appele la fonction removeModal() qui ferme la modale.*/
 cancel[0].addEventListener("click", () =>{
     removeModal();
 })
 
+
+add.addEventListener("click", () =>{
+    let rating = document.getElementById("rating").value;
+    if (rating === "" || rating > 5){
+        alert("Please enter valid values (rating between 1 and 5).")
+    }
+
+})
 
