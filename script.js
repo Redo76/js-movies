0d9ade262979db5509e9a745b35e02502237ad65
Redo76 <redouane.hamadache1@gmail.com>
2022-04-11 14:02:30 +0200

    Ajout des movies a moitié terminé et debut de la modal de suppression

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-// Déclaration des variables :
+// Déclaration des variables ************************************************************************************
 const main = document.querySelector("main");
 const backdrop = document.getElementById("backdrop");
 const addMovie = document.getElementById("addMovie");
-const modal = document.getElementById("add-modal");
+const addModal = document.getElementById("add-modal");
+const suppMovie = document.createElement("div");
+const deleteModal = document.getElementById("delete-modal");
+
 const cancel = document.getElementsByClassName("btn--passive");
 const add = document.querySelector(".btn--success");
 const entryText = document.getElementById("entry-text");
+const section = document.createElement("section");
 
 
-// Déclaration des fonctions :
+// Déclaration des fonctions *************************************************************************************
 
 /*On crée une fonction qui permet de fermer la modal */
 function removeModal() {
-    modal.style.display = "none";
+    addModal.style.display = "none";
     backdrop.style.display = "none";
 }
 
@@ -24,34 +28,47 @@ function ajouterMovie() {
     let title = document.getElementById("title").value;
     let imageUrl = document.getElementById("image-url").value;
     let rating = document.getElementById("rating").value;
-    const section = document.createElement("section");
     
+
     main.appendChild(section);
-    section
-    section.classList.add("#entry-text");
-    section.innerHTML = `${imageUrl} ${title} ${rating}`;
+    section.setAttribute("class", "movie-element");
+    section.setAttribute("id", "entry-text");
+    
+    const img = document.createElement("img");
+    img.setAttribute("class", "movie-element__image");
+    img.innerHTML = imageUrl;
+    section.appendChild(img);
+
+    const h2 = document.createElement("h2");
+    h2.setAttribute("class", "h2");
+    h2.innerHTML = title;
+    section.appendChild(h2);
+
+    const p = document.createElement("p");
+    p.setAttribute("class", "p");
+    p.innerHTML = `${rating}/5 stars`;
+    section.appendChild(p);
+
+    suppMovie.style.width = "30%";
+    suppMovie.style.height = "100%";
+    section.appendChild(suppMovie);
+
     removeModal();
 }
 
-// Début du code :
-
-// /*Lorsqu'on clique sur ADD MOVIE la modale ainsi que le fond transparent apparait.*/
-// addMovie.addEventListener("click", () =>{
-//     backdrop.style.display = "block";
-//     modal.style.display = "block";
-// })
+// Début du code ****************************************************************************************************
 
 /* Lorqu'on clique sur le bouton cancel de la modal, on appele la fonction removeModal() qui ferme la modal.*/
-cancel[0].addEventListener("click", () =>{
+cancel[0].addEventListener("click", () =>{  // cancel[0] est le premier bouton du tableau cancel.
     removeModal();
 })
 
 /* Lorsqu'on clique sur la page web*/
 document.addEventListener("click", (e) =>{
     /*Lorsqu'on clique sur ADD MOVIE, la modale ainsi que le fond transparent apparait.*/
-    if (e.target == addMovie){
+    if (e.target === addMovie){
         backdrop.style.display = "block";
-        modal.style.display = "block";
+        addModal.style.display = "block";
     } 
     /* Lorsque'on clique sur le bouton cancel OU que l'on clique sur n'importe quel élément qui n'est pas la modal, on ferme la modal.*/
     else if (e.target == cancel || !e.target.closest("#add-modal")){ // La méthode closest récupére l'ancetre le plus proche ou le parent de l'élément correspondant aux sélecterus
@@ -61,12 +78,21 @@ document.addEventListener("click", (e) =>{
 
 add.addEventListener("click", () =>{
     let rating = document.getElementById("rating").value;
-    if (rating == "" || rating > 5){
+    if (rating <= 0 || rating > 5){
         alert("Please enter valid values (rating between 1 and 5).")
     } else {
         retirerMovie();
         ajouterMovie();
     }
+    
+})
+
+section.addEventListener("click", () =>{
+    deleteModal.style.display = "block";
+    backdrop.style.display = "block";
+})
+
+cancel[1].addEventListener("click", () =>{
     
 })
 
